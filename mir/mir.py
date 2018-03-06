cd5ef9d317805c9ff80613a2f9bd9963eb80c0a4
Steven Brien <sbrien@hlkagency.com>
2018-03-06 15:46:28 -0600

    Fix app reloading

--- mir/mir.py
+++ mir/mir.py
@@ -26,20 +26,10 @@ from lib.blueprints import blueprint_factory
 from lib.bootstrap import create_admin
 from lib.filestore import CloudinaryMediaStorage
 
-# Set up Path for application
-settings_path = os.path.join(os.getcwd())
-sys.path.insert(0, settings_path)
-
-# Get settings
-settings = get_settings_dict()
-
-# Set up Media
-media = settings['MEDIA'] if 'MEDIA' in settings else GridFSMediaStorage
 
 # ------------------------------
 # Initialize authentication
 # ------------------------------
-
 
 class BasicAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource, method):
@@ -80,64 +70,23 @@ class MetaValidation(Validator):
 # Initialize Application
 # ------------------------------
 
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
+def init_app(reload=False):
+    # Set up Path for application
+    settings_path = os.path.join(os.getcwd())
+    sys.path.insert(0, settings_path)
 
+    # Get settings
+    settings = get_settings_dict()
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    # Set up Media
+    media = settings['MEDIA'] if 'MEDIA' in settings else GridFSMediaStorage
 
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
-def start_app(reload=False):
     # Set up Eve Application
     settings['DOMAIN'] = get_models()
 
-    app = Eve(
-        settings=settings,
-        auth=BasicAuth,
-        validator=MetaValidation,
-        static_folder=os.path.join(settings_path, 'static'),
-        media=media
-    )
-    create_admin(app, generate_token)
-
-    template_loader = jinja2.ChoiceLoader([
-        app.jinja_loader,
-        jinja2.FileSystemLoader([os.path.join(settings_path, 'templates')]),
-    ])
-    app.jinja_loader = template_loader
-
-    CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-    hooks_factory(app)
-    blueprint_factory(app)
-
-    # Run with Gunicorn
-    options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'workers': number_of_workers(),
-        'reload': False
-    }
-    StandaloneApplication(app, options).run()
-
-
-def create_app():
-    # Set up Eve Application
-    settings['DOMAIN'] = get_models()
-    settings['PUBLIC_METHODS'] = ['GET']
-    settings['PUBLIC_ITEM_METHODS'] = ['GET']
+    if not reload:
+        settings['PUBLIC_METHODS'] = ['GET']
+        settings['PUBLIC_ITEM_METHODS'] = ['GET']
 
     app = Eve(
         settings=settings,
@@ -159,3 +108,41 @@ def create_app():
     blueprint_factory(app)
 
     return app
+
+
+def number_of_workers():
+    return (multiprocessing.cpu_count() * 2) + 1
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, initializer, reload, options=None):
+        self.options = options or {}
+        self.initializer = initializer
+        self.reload = reload
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = dict([(key, value) for key, value in iteritems(self.options)
+                       if key in self.cfg.settings and value is not None])
+        for key, value in iteritems(config):
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        app = self.initializer(self.reload)
+        return app
+
+
+def start_app(reload=False):
+
+    # Run with Gunicorn
+    options = {
+        'bind': '%s:%s' % ('127.0.0.1', '8080'),
+        'workers': 1 if reload else number_of_workers(),
+        'reload': reload
+    }
+    StandaloneApplication(init_app, reload, options).run()
+
+
+def create_app():
+    return init_app()
