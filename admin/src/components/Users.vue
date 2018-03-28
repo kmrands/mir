3143f2c94ac495c24ebbb26e2dd050b05526b6f8
Steven Brien <sbrien@hlkagency.com>
2018-03-28 10:40:08 -0500

    Users component template

---
+++ admin/src/components/Users.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="users">
+    <div class="row align-center">
+      <div class="columns small-12 medium-8 secondary-menu padding-lg">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as R from 'ramda'
+
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Users',
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    ...mapActions(['getCurrentCollection']),
+  },
+  computed: {
+    ...mapGetters(['currentCollection']),
+  },
+}
+</script>
+
+<style lang="scss">
+  @import '../scss/settings';
+</style>
