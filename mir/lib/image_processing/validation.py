93240710f5f7b8ef95d228bdb350b75c93dcd67a
Steven Brien <sbrien@hlkagency.com>
2018-04-02 15:29:57 -0500

    Code cleanup and organization

---
+++ mir/lib/image_processing/validation.py
@@ -0,0 +1,60 @@
+# ---------------------------------
+# Validation Schema
+# ---------------------------------
+
+def to_bool(value):
+    return value.lower() == 'true'
+
+schema = {
+    'format': {
+        'type': 'string',
+        'allowed': [
+            'JPEG',
+            'GIF',
+            'PNG'
+        ]
+    },
+    'quality': {
+        'type': 'integer',
+        'coerce': int
+    },
+    'optimize': {
+        'type': 'boolean',
+        'coerce': to_bool
+    },
+    'thumbnail': {
+        'type': 'string',
+        'regex': '^[0-9]+,[0-9]+$'
+    },
+    'rotate': {
+        'type': 'integer',
+        'coerce': int
+    },
+    'blur': {
+        'type': 'float',
+        'coerce': float
+    },
+    'contrast': {
+        'type': 'float',
+        'coerce': float
+    },
+    'brightness': {
+        'type': 'float',
+        'coerce': float
+    },
+    'saturation': {
+        'type': 'float',
+        'coerce': float
+    },
+    'sharpness': {
+        'type': 'float',
+        'coerce': float
+    },
+    'flip': {
+        'type': 'string',
+        'allowed': [
+            'horizontal',
+            'vertical',
+        ]
+    }
+}
