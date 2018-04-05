bb7a17c3e37e9d72fd3052e3a3083de7a321c254
Steven Brien <sbrien@hlkagency.com>
2018-04-05 12:54:36 -0500

    Update settings templates

--- mir/mir.py
+++ mir/mir.py
@@ -36,8 +36,6 @@ from config import APP_DIR
 
 class jwtAuth(TokenAuth):
     def check_auth(self, token, allowed_roles, resource, method):
-        # Patch, because what the fuck eve?
-        # TODO: Why the fuck would eve make our token lowercase?
         token = request.headers.get('Authorization')
         if token:
             try:
