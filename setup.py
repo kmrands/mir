92582aad4ca784d6e3cc279f85afa32ce5305a0f
Steven Brien <sbrien@hlkagency.com>
2018-04-15 11:10:32 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ test_requirements = [
 
 setup(
     name='mir',
-    version='0.3.13',
+    version='0.3.14',
     description="Mir API Framework",
     long_description=readme + '\n\n' + history,
     author="Steven Brien",
