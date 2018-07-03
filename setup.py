9b2cb13695fa338458d7fc1cca9c58f5eec36a56
Steven Brien <spbrien@gmail.com>
2018-07-03 10:41:51 -0500

    Build latest admin, bump version

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ test_requirements = [
 
 setup(
     name='mir',
-    version='1.1.6',
+    version='1.1.7',
     description="Mir API Framework",
     long_description=readme + '\n\n' + history,
     author="Steven Brien",
